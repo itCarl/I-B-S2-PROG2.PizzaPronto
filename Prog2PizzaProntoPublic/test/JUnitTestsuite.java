20baf13969e30698e5ea285d2d3f21cf2eded3a2
MewesM <mewes.maximilian@gmx.de>
2019-04-12 12:44:38 +0200

    Added jUnitTest Classes

---
+++ Prog2PizzaProntoPublic/test/JUnitTestsuite.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ JUnitTestChefVO.class, JUnitTestCustomerVO.class,
+		JUnitTestPizzaVO.class })
+public class JUnitTestsuite {
+}
